b4fbc2d25f00e938b5fdb6f6f0b29a56c1faccf8
flow6852 <flow6852math@gmail.com>
2023-04-14 01:09:16 +0900

    change kind

--- denops/@ddu-sources/vim_option.ts
+++ denops/@ddu-sources/vim_option.ts
@@ -11,7 +11,7 @@ type Params = {
 }
 
 export class Source extends BaseSource<Params> {
-  override kind = "vim_variable";
+  override kind = "vim_type";
 
   override gather(args: {
     denops: Denops;
