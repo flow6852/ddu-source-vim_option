0b3beb306f21690252b1e223ba21585d6205763f
flow6852 <flow6852math@gmail.com>
2023-07-23 12:28:52 +0900

    Update

--- denops/@ddu-sources/vim_option.ts
+++ denops/@ddu-sources/vim_option.ts
@@ -3,8 +3,8 @@ import {
   Context,
   Item,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v3.0.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.0.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.4.3/deps.ts";
 
 // type Params = Record<never, never>;
 type Params = {
